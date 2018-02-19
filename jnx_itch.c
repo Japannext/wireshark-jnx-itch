f9765acea63a3472d19c30eb2696a6aae33973d1
Merdan Atajanov <merdan.atajanov@japannext.co.jp>
2018-02-20 08:16:10 +0900

    Other: Update library link and do not mention versions of JNX protocols
    Equities, bonds and currencies have different protocol versions.

--- jnx_itch.c
+++ jnx_itch.c
@@ -8,7 +8,7 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  *
  * Documentation:
- * https://www.japannext.co.jp/en/pub_data/pub_onboarding/Japannext_PTS_ITCH_v1.4.pdf
+ * https://www.japannext.co.jp/library/
  */
 
 #ifdef HAVE_CONFIG_H
